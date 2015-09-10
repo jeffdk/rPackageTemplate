391239826080759f23fda195e74dcac92853fbec
Jeff Kaplan <jeffreydkaplan@gmail.com>
2015-09-09 18:59:36 -0700

    Fill out template files

--- R/replacementKeys.R
+++ R/replacementKeys.R
@@ -1,4 +1,7 @@
 #' List of replacement keys
 #'
 #' @format A simple character vector.
-replacementKeys <- c("package_name")
+replacementKeys <- c("package_name",
+                     "first_name",
+                     "last_name",
+                     "email")
---
+++ templates/README.md
@@ -0,0 +1,5 @@
+
+# ${package_name}
+
+This is the readme for ${package_name}
+Contact: ${email}
